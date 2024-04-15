607a5fe1a2db2a1def850b44a0c32fc5e58dff48
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-04-15 14:13:40 +0530

    check if two strings are equalent

---
+++ index5.js
@@ -0,0 +1,17 @@
+//check if two strings are equalent
+
+//word1 = abc  (join(''))
+//word2 = abc  (join(''))
+
+var arrayStringsAreEqual = function(word1, word2) {
+   var d = word1.join('')
+   var e = word2.join('')
+   if(d==e){
+    return true
+   }
+   return false
+};
+const word1 = ["ab", "c"]
+const word2 = ["a", "bc"]
+
+console.log(arrayStringsAreEqual(word1,word2))
