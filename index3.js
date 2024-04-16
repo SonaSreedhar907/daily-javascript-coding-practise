22b5a6e64308a45353a8709e2614f39c9376ae21
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-04-16 19:21:01 +0530

    reverse a string

--- index3.js
+++ index3.js
@@ -1,16 +1,18 @@
 //number of common factors
 
+//a=12
+//b=6
+//common factors are 1,2,3,6 ans is 4
 
-var commonFactors = function(a, b) {
+
+var commonfactors = function(a,b){
    let count=0
-   let c = Math.min(a,b)
-   for(i=1;i<=c;i++){
-     if(a%i==0 && b%i==0){
-        count++
-     }
-     
+   const c=Math.min(a,b)
+   for(let i=1;i<=c;i++){
+      if(a%i==0 && b%i==0){
+         count++
+      }
    }
    return count
-
 }
-console.log(commonFactors(12,6))
+console.log(commonfactors(12,6))
