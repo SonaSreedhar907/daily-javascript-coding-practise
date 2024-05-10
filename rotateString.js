554416c8d75a650c03b03f21830e9fbeba5bc746
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-05-10 12:53:27 +0530

    rotateString

---
+++ rotateString.js
@@ -0,0 +1,11 @@
+
+
+var rotateString = function(s,goal){
+  if(s.length !== goal.length){
+    return false
+  }
+  return (s+s).includes(goal)
+}
+const s ='abcde'
+const goal = 'cdeeb'
+console.log(rotateString(s,goal))
