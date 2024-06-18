447b74b3dfbc2125c395fc2e643d5742fc6eface
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-06-18 22:45:02 +0530

    interview.js added

---
+++ interview.js
@@ -0,0 +1,43 @@
+// const obj1={first:10,second:20,third:30}
+// console.log(obj1)
+
+
+// const obj1 = {first: 20, second: 30, first: 50};
+// console.log(obj1);
+
+
+// console.log(typeof(NaN))
+
+
+
+// const example = ({a,b,c})=>{
+//     console.log(a,b,c)
+// }
+// example(0,1,2)
+
+
+
+
+// var a = Math.max()
+// var b = Math.min()
+// console.log(a)
+// console.log(b)
+
+
+// var a = true+true+true*3
+// console.log(a)
+
+
+// function test(...args){
+//     console.log(args)
+//     console.log(typeof args)
+// }
+// test(2)
+
+
+// a=[1,2,3,4,5]
+// console.log(a.slice(2,4))
+
+
+
+// console.log(NaN === NaN)
