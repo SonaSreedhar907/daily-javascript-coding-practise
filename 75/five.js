d65822289c7764cff7182bf9d05a9d2f9590058f
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-07-10 22:28:55 +0530

    counter

--- 75/five.js
+++ 75/five.js
@@ -1,19 +1,34 @@
 //counter
 
 
+// var createCounter = function(n){
+//   let count = 1
+//   return function(){
+//     if(count === 1){
+//         count++
+//         return n
+//     }else{
+//         n=n+1
+//         return n
+//     }
+//   }
+// }
+// const counter = createCounter(10)
+// console.log(counter())
+// console.log(counter())
+// console.log(counter())
+
+
+
 var createCounter = function(n){
-  let count = 1
-  return function(){
-    if(count === 1){
-        count++
-        return n
-    }else{
-        n=n+1
-        return n
+    return function(){
+      return ++n
     }
   }
-}
-const counter = createCounter(10)
-console.log(counter())
-console.log(counter())
-console.log(counter())
+  const counter = createCounter(10)
+  console.log(counter())
+  console.log(counter())
+  console.log(counter())
+
+
+  //check n=n+1, n+=1, n++, ++n
